70466e9e264f72fe7182fddf45797909fad24396
Caleb Maclennan <caleb@alerque.com>
2015-07-20 21:15:05 +0300

    add script for printing Dalyan Gumruk late notices

---
+++ bin/pdgln.zsh
@@ -0,0 +1,46 @@
+#!/usr/bin/env zsh
+
+while getopts d:t: opt; do
+	case $opt in
+		d) date=$OPTARG;;
+		t) time=$OPTARG;;
+	esac
+done
+
+: ${date:=today}
+: ${time:=21:15}
+
+export LANG=tr_TR.UTF8
+
+BUILDDIR=$(mktemp --dir --tmpdir)
+trap 'rm -rf $BUILDDIR' EXIT SIGHUP SIGTERM
+pushd $BUILDDIR
+
+for i in $(seq 0 1); do
+	formatdate=$(date --date "$date + $i days" "+%A günü %Y-%m-%d")
+	> ${i}.sil <<- EOF
+		\begin[papersize=a5]{document}
+		\nofolios
+		\begin[first-content-frame=content]{pagetemplate}
+		\frame[id=content, top=10%, bottom=90%, left=10%, right=80%]
+		\end{pagetemplate}
+		\set[parameter=current.parindent,value=0]
+		\set[parameter=document.parindent,value=0]
+		\font[language=tr,family=Crimson,size=13pt]
+
+		Dalyan Gümrük İşhanı'nın dikkatine:
+		\bigskip
+
+		$formatdate tarihinde 802 numaralı ofiste akşam \font[weight=800]{saat $time} olana kadar çalışıyor olacağım.
+		\skip[height=3em]
+
+		Caleb Maclennan
+
+		\end{document}
+	EOF
+
+	sile ${i}.sil -o ${i}.pdf
+done
+
+pdfjam 0.pdf 1.pdf --nup 2x1 --landscape --outfile 2up.pdf
+lpr 2up.pdf
