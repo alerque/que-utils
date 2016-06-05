b797f134e41cfd47fba1986dff8407d51ffd9a9d
Caleb Maclennan <caleb@alerque.com>
2016-06-05 11:11:14 +0300

    Add day increment setter to late notice printer

--- bin/pdgln.zsh
+++ bin/pdgln.zsh
@@ -1,14 +1,16 @@
 #!/usr/bin/env zsh
 
-while getopts d:t: opt; do
+while getopts d:t:i: opt; do
 	case $opt in
 		d) date=$OPTARG;;
 		t) time=$OPTARG;;
+		i) inc=$OPTARG;;
 	esac
 done
 
 : ${date:=today}
 : ${time:=21:15}
+: ${inc:=1}
 
 export LANG=tr_TR.UTF8
 
@@ -16,7 +18,7 @@ BUILDDIR=$(mktemp --dir --tmpdir)
 trap 'rm -rf $BUILDDIR' EXIT SIGHUP SIGTERM
 pushd $BUILDDIR
 
-for i in $(seq 0 1); do
+for i in 0 ${inc}; do
 	formatdate=$(date --date "$date + $i days" "+%A günü %F")
 	> ${i}.sil <<- EOF
 		\begin[papersize=a5]{document}
@@ -26,12 +28,12 @@ for i in $(seq 0 1); do
 		\end{pagetemplate}
 		\set[parameter=current.parindent,value=0]
 		\set[parameter=document.parindent,value=0]
-		\font[language=tr,family=Crimson,size=14pt]
+		\font[language=tr,family=Crimson,size=14pt,weighth=400,style=Roman]
 
 		Dalyan Gümrük İşhanı'nın dikkatine:
 		\bigskip
 
-		$formatdate tarihinde 802 numaralı ofiste akşam \font[weight=800]{saat $time} olana kadar çalışıyor olacağım.
+		$formatdate tarihinde 802 numaralı ofiste akşam \font[weight=800,style=Bold]{saat $time} olana kadar çalışıyor olacağım.
 		\skip[height=4em]
 
 		Caleb Maclennan
@@ -42,5 +44,5 @@ for i in $(seq 0 1); do
 	sile ${i}.sil -o ${i}.pdf
 done
 
-pdfjam 0.pdf 1.pdf --nup 2x1 --landscape --outfile 2up.pdf
+pdfjam 0.pdf ${inc}.pdf --nup 2x1 --landscape --outfile 2up.pdf
 lpr 2up.pdf
