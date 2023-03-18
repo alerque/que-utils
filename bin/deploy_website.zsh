543af8bd564b04defceedffff75c7fa4cfca2186
Caleb Maclennan <caleb@alerque.com>
2023-03-18 22:03:51 +0100

    Add webpc related content directories to deploy logic

--- bin/deploy_website.zsh
+++ bin/deploy_website.zsh
@@ -28,6 +28,8 @@ rsync -crlpv --delete \
     --exclude "makefile" --exclude "Makefile" --exclude ".gitlab-ci.yml" \
     --include "wp-content/uploads/.htaccess" \
     --exclude "wp-content/uploads" \
+    --include "wp-content/uploads-webpc/.htaccess" \
+    --exclude "wp-content/uploads-webpc" \
     --exclude "wp-content/cache" \
     --exclude "wp-content/et-cache" \
     --include "wp-content/w3tc-config/master.php" \
@@ -46,7 +48,7 @@ pushd "$deploydir"
 
 [[ -f wp-config.php ]] && mkdir -p wp-content/uploads ||:
 
-for dir in wp-content/uploads wp-content/gallery wp-content/cache wp-content/w3tc-config wp-content/webp-express; do
+for dir in wp-content/uploads wp-content/uploads-webpc wp-content/gallery wp-content/cache wp-content/w3tc-config wp-content/webp-express; do
     test -d "$dir" || continue
     sudo chown -R http:http "$dir"
     find "$dir" -type f -execdir sudo chmod -x {} \+
